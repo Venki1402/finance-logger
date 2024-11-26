74201630152a5a23e25d9c74238889aabf40f4bf
venki-allen <venkatesh.alampally@allen.in>
2024-11-26 15:44:27 +0530

    forever on -> -w (watch)
    Signed-off-by: venki-allen <venkatesh.alampally@allen.in>

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+// to automate tsc script.ts -w
 var villagerA = "rio";
 console.log(villagerA);
 var userInputs = document.querySelectorAll("input");
 console.log(userInputs);
+var villagerB = villagerA;
+console.log(villagerB);
--- script.ts
+++ script.ts
@@ -1,5 +1,10 @@
+// to automate tsc script.ts -w
+
 const villagerA = "rio";
 console.log(villagerA);
 
 const userInputs = document.querySelectorAll("input");
 console.log(userInputs);
+
+const villagerB = villagerA;
+console.log(villagerB);
